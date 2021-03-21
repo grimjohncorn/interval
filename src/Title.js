46c3db9c9fdf79fea68e64f170551c72a1979fd9
chris grimley <chrismessage55@outlook.com>
2021-03-21 16:18:27 +0000

    add first letter effects to title

--- src/Title.js
+++ src/Title.js
@@ -1,7 +1,16 @@
 const Title = ({ title }) => {
+    const titleWords = title.split(' ')
+    
+    let formattedWords = []
+    titleWords.forEach((word, index, words) => {
+        formattedWords.push(<span className="firstLetter">{word.charAt(0)}</span>)
+        formattedWords.push(word.slice(1,word.length))
+        if(index !== words.length-1) formattedWords.push(" ") //add space between words
+    })
+    
     return (
         <div className='title'>
-            <h1>{title}</h1>
+            <h1>{formattedWords.map(item => item)}</h1>
         </div>
       )
 }
